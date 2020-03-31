ff32c2c2800fb31b020a0a193c88723a56037a2c
Jeremy Leipzig <leipzig@gmail.com>
2020-03-31 15:17:27 -0400

    just slicer

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@ VERSION="latest"
 
 docker build -t stevepieper/base:$VERSION base
 docker build -t stevepieper/x11:$VERSION x11
-docker build -t stevepieper/slicer-dev:$VERSION slicer-dev
-docker build -t stevepieper/slicer3:$VERSION slicer3
-docker build -t stevepieper/slicer:$VERSION slicer
+#docker build -t stevepieper/slicer-dev:$VERSION slicer-dev
+#docker build -t stevepieper/slicer3:$VERSION slicer3
+docker build -t leipzig/slicer:$VERSION slicer
 
-docker build --no-cache -t stevepieper/slicer-chronicle:$VERSION slicer-chronicle
+#docker build --no-cache -t stevepieper/slicer-chronicle:$VERSION slicer-chronicle
 
-docker build -t stevepieper/slicer-dev:$VERSION slicer-dev
+#docker build -t stevepieper/slicer-dev:$VERSION slicer-dev
